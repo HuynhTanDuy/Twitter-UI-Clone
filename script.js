f1b19bcea8340075c3a4d042bc96984818170ec8
DuyHuynh <tanduyht@gmail.com>
2023-06-27 20:12:38 +0700

    add js to go to feeds page

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ middleContent.addEventListener('click', e => {
     console.log(e.target);
     goToLoginPage();
 });
+
+const logInBtn = document.querySelector('.log-in-btn');
+logInBtn.addEventListener('click', () => {
+  const username = document.querySelector('.username-input');
+  const password = document.querySelector('.password-input');
+  console.log('Username is ' + username + ". Password is " + password);
+  if (username.value !== '' && password.value !== '') {
+      console.log('Go to feeds page')
+      mainPage.style.display = 'none';
+      feedsPage.style.display = 'grid';
+  } else {
+      goToLoginPage();
+  }
+});
